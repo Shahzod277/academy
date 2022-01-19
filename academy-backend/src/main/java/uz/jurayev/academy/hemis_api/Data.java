1eddcd201c512520ac8c99e83fb4569fa7fe4b54
Shahzod277 <shahzodozbekov7@gmail.com>
2022-01-19 10:46:02 +0500

    added student controller and service!

--- academy-backend/src/main/java/uz/jurayev/academy/hemis_api/Data.java
+++ academy-backend/src/main/java/uz/jurayev/academy/hemis_api/Data.java
@@ -2,7 +2,7 @@ package uz.jurayev.academy.hemis_api;
 
 import uz.jurayev.academy.rest.StudentInfoDto;
 
-public class ResponseData {
+public class Data {
 	private String code;
 	private StudentInfoDto studentInfo;
 	private Boolean success;
